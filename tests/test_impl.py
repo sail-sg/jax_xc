5191755e9da3ade932a31edadd8ac11376efb145
Kunhao Zheng <zhengkh@sea.com>
2022-12-15 18:30:38 +0800

    add test_impl pol

--- tests/test_impl.py
+++ tests/test_impl.py
@@ -20,6 +20,7 @@ from absl.testing import absltest, parameterized
 from absl import logging
 import numpy as np
 import time
+from jax.tree_util import Partial
 
 from jax_xc import libxc as pylibxc
 from jax_xc.libxc import libxc
@@ -65,83 +66,104 @@ class _TestGetParams(parameterized.TestCase):
   @parameterized.parameters(
     *(pylibxc.util.xc_available_functional_names()),
   )
-  def test_get_params_unpol(self, name):
+  def test_impl(self, name):
 
-    def inp_to_impl_args(inp, fn_type):
-      if fn_type == "lda":
-        return (inp["rho"],)
-      elif fn_type == "gga":
-        return (inp["rho"], inp["sigma"])
-      elif fn_type == "mgga":
-        return (inp["rho"], inp["sigma"], inp["lapl"], inp["tau"])
+    def get_impl_fn_and_inputs(inputs, impl, fn_type, p, polarized):
+      rho0, rho1, sigma0, sigma1, sigma2, lapl0, lapl1, tau0, tau1 = inputs
+      impl_fn = jax.vmap(Partial(impl, params=p.params, p=p))
 
-    def jit_impl_fn(impl, fn_type, p):
-      if fn_type == "lda":
-        return jax.jit(jax.vmap(lambda x0: impl(x0, p.params, p), in_axes=(0,)))
-      elif fn_type == "gga":
-        return jax.jit(
-          jax.vmap(lambda x0, x1: impl(x0, x1, p.params, p), in_axes=(0, 0))
-        )
-      elif fn_type == "mgga":
-        return jax.jit(
-          jax.vmap(
-            lambda x0, x1, x2, x3: impl(x0, x1, x2, x3, p.params, p),
-            in_axes=(0, 0, 0, 0)
+      if polarized:
+        libxc_input_args = {
+          "rho": jnp.stack([rho0, rho1], -1),
+          "sigma": jnp.stack([sigma0, sigma1, sigma2], -1),
+          "lapl": jnp.stack([lapl0, lapl1], -1),
+          "tau": jnp.stack([tau0, tau1], -1),
+        }
+        if fn_type == "lda":
+          fn_input_args = (rho0, rho1)
+
+        elif fn_type == "gga":
+          fn_input_args = (rho0, rho1, sigma0, sigma1, sigma2)
+
+        elif fn_type == "mgga":
+          fn_input_args = (
+            rho0, rho1, sigma0, sigma1, sigma2, lapl0, lapl1, tau0, tau1
           )
-        )
+      else:
+        rho = rho0 + rho1
+        sigma = sigma0 + sigma2 + 2 * sigma1
+        lapl = lapl0 + lapl1
+        tau = tau0 + tau1
+        libxc_input_args = dict(rho=rho, sigma=sigma, lapl=lapl, tau=tau)
+        if fn_type == "lda":
+          fn_input_args = (rho,)
+        elif fn_type == "gga":
+          fn_input_args = (rho, sigma)
+        elif fn_type == "mgga":
+          fn_input_args = (rho, sigma, lapl, tau)
+      return impl_fn, fn_input_args, libxc_input_args
 
-    func = pylibxc.LibXCFunctional(name, 1)
-    x = ctypes.cast(func.xc_func, ctypes.c_void_p)
-    p = libxc.get_p(x.value)
-    name = p['name']
-    if name in SKIP_LIST:
-      logging.info(f"Skipping {name} due to existing in SKIP_LIST")
-      return
-    p = utils.dict_to_namedtuple(p, "P")
-    if not hasattr(impl, p.name):
-      logging.info(f"Skipping {p.name} due to no maple code implementation")
-      return
-    fn_type = utils.functional_name_to_type(name)
+    for polarized in [False, True]:
 
-    batch = 100
-    seed = 0
-    key = jax.random.PRNGKey(seed)
-    keys = jax.random.split(key, 4)
-    inp = {}
-    inp["rho"] = jax.random.uniform(
-      keys[0], (batch,), dtype=jnp.float64, minval=1e-10, maxval=1e2
-    )
-    inp["sigma"] = jax.random.uniform(
-      keys[1], (batch,), dtype=jnp.float64, minval=1e-10, maxval=1e2
-    )
-    inp["lapl"] = jax.random.uniform(
-      keys[2], (batch,), dtype=jnp.float64, minval=1e-10, maxval=1e2
-    )
-    inp["tau"] = jax.random.uniform(
-      keys[3], (batch,), dtype=jnp.float64, minval=1e-10, maxval=1e2
-    )
-    start_time = time.time()
-    res1 = func.compute(inp)
-    end_time = time.time()
-    logging.info(f"pylibxc {name} took {end_time - start_time} seconds")
-    res1_zk = res1["zk"].squeeze()
+      func = pylibxc.LibXCFunctional(name, int(polarized) + 1)
+      x = ctypes.cast(func.xc_func, ctypes.c_void_p)
+      p = libxc.get_p(x.value)
+      name = p['name']
+      if name in SKIP_LIST:
+        logging.info(f"Skipping {name} due to existing in SKIP_LIST")
+        return
+      p = utils.dict_to_namedtuple(p, "P")
+      if not hasattr(impl, p.name):
+        logging.info(f"Skipping {p.name} due to no maple code implementation")
+        return
+      fn_type = utils.functional_name_to_type(name)
 
-    impl_fn = getattr(impl, p.name).unpol
-    impl_fn = jit_impl_fn(impl_fn, fn_type, p)
+      batch = 100
+      seed = 0
+      key = jax.random.PRNGKey(seed)
+      keys = jax.random.split(key, 4)
+      # 9 represent the polarized cases:
+      # r0, r1, s0, s1, s2, l0, l1, t0, t1
+      inputs = jax.random.uniform(
+        keys[0], (9, batch), dtype=jnp.float64, minval=1e-5, maxval=1e2
+      )
+      # sigma should have: s0 + s2 - 2 * s1 >= 0
+      # update slice of inputs[2 : 5, :] to 1 if
+      # inputs[2] + inputs[4] - 2 * inputs[3] < 0
+      inputs = jax.ops.index_update(
+        inputs, jax.ops.index[2:5, :],
+        jnp.where(inputs[2] + inputs[4] - 2 * inputs[3] < 0, 1, inputs[2:5])
+      )
 
-    start_time = time.time()
-    res2_zk = impl_fn(*inp_to_impl_args(inp, fn_type))
-    end_time = time.time()
-    logging.info(
-      f"jax_xc {name} took {end_time - start_time} compilation seconds"
-    )
-    res2_zk = impl_fn(*inp_to_impl_args(inp, fn_type))
-    snd_end_time = time.time()
-    logging.info(
-      f"jax_xc {name} took {snd_end_time - end_time} execution seconds"
-    )
-    # absolute(res2_zk - res1_zk) <= (atol + rtol * absolute(res1_zk)
-    np.testing.assert_allclose(res2_zk, res1_zk, rtol=THRESHOLD, atol=THRESHOLD)
+      impl_module = getattr(impl, p.name)
+      impl_fn = impl_module.pol if polarized else impl_module.unpol
+      impl_fn, input_args, libxc_input_args = get_impl_fn_and_inputs(
+        inputs, impl_fn, fn_type, p, polarized
+      )
+      impl_fn = jax.jit(impl_fn)
+
+      start_time = time.time()
+      res2_zk = impl_fn(*input_args)
+      end_time = time.time()
+      logging.info(
+        f"jax_xc {name} took {end_time - start_time} compilation seconds"
+      )
+      res2_zk = impl_fn(*input_args)
+      snd_end_time = time.time()
+      logging.info(
+        f"jax_xc {name} took {snd_end_time - end_time} execution seconds"
+      )
+
+      start_time = time.time()
+      res1 = func.compute(libxc_input_args)
+      end_time = time.time()
+      logging.info(f"pylibxc {name} took {end_time - start_time} seconds")
+      res1_zk = res1["zk"].squeeze()
+
+      # absolute(res2_zk - res1_zk) <= (atol + rtol * absolute(res1_zk)
+      np.testing.assert_allclose(
+        res2_zk, res1_zk, rtol=THRESHOLD, atol=THRESHOLD
+      )
 
   @parameterized.parameters(
     *pylibxc.util.xc_available_functional_numbers(),
