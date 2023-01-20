37318cb709bec24f0cde0b064fdd7924becf4ea3
Zheng Kunhao <zhengkh@sea.com>
2023-01-20 13:48:25 +0800

    license

--- tests/test_impl.py
+++ tests/test_impl.py
@@ -1,16 +1,8 @@
 # Copyright 2022 Garena Online Private Limited
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import jax
 import ctypes
@@ -20,6 +12,7 @@ from absl.testing import absltest, parameterized
 from absl import logging
 import numpy as np
 from jax.tree_util import Partial
+import pandas as pd
 
 from jax_xc import libxc as pylibxc
 from jax_xc.libxc import libxc
@@ -62,10 +55,7 @@ SKIP_LIST = [
 
 class _TestImpl(parameterized.TestCase):
 
-  @parameterized.parameters(
-    *(pylibxc.util.xc_available_functional_names()),
-  )
-  def test_impl(self, name):
+  def test_impl(self):
 
     def get_impl_fn_and_inputs(inputs, impl, fn_type, p, polarized):
       rho0, rho1, sigma0, sigma1, sigma2, lapl0, lapl1, tau0, tau1 = inputs
