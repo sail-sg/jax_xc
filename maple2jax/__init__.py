fc2c463571ea8a90f5906f65b6be308be2c146a9
mavenlin <mavenlin@gmail.com>
2023-05-22 21:36:08 +0800

    Update __init__.py
    bump the version for release

--- maple2jax/__init__.py
+++ maple2jax/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Garena Online Private Limited
+# Copyright 2023 Garena Online Private Limited
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -6,4 +6,4 @@
 
 from .functionals import *  # noqa
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
