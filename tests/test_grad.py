5c82ec9dde18fa09fb722e8877a4147594b8efd6
Zekun Shi <derek.zekun.shi@gmail.com>
2023-07-20 14:03:26 +0800

    update test

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -13,19 +13,39 @@ from absl.testing import absltest, parameterized
 
 class _TestGrad(parameterized.TestCase):
 
-  def test_grad_is_not_nan(self):
+  @parameterized.parameters(
+    (jax_xc.lda_x,),
+    (jax_xc.gga_x_pbe,),
+  )
+  def test_not_nan(self, xc_fn_fac):
     """Test that the gradient of the functional is not nan."""
-    lda = jax_xc.lda_x(polarized=False)
+    xc_fn = xc_fn_fac(polarized=False)
     r = jnp.array([[10., 10., 10.], [9, 9, 9]])
 
     def rho(r):
       return jnp.prod(jax.scipy.stats.norm.pdf(r, loc=0, scale=1))
 
     def grad(r):
-      return jax.grad(lda, argnums=1)(rho, r)
+      return jax.grad(xc_fn, argnums=1)(rho, r)
 
+    v = jax.vmap(lambda r: xc_fn(rho, r))(r)
+    self.assertFalse(jnp.isnan(v).any())
     g = jax.vmap(grad)(r)
     self.assertFalse(jnp.isnan(g).any())
+
+  # def test_grad_is_not_nan(self):
+  #   """Test that the gradient of the functional is not nan."""
+  #   xc_fn = jax_xc.lda_x(polarized=False)
+  #   r = jnp.array([[10., 10., 10.], [9, 9, 9]])
+
+  #   def rho(r):
+  #     return jnp.prod(jax.scipy.stats.norm.pdf(r, loc=0, scale=1))
+
+  #   def grad(r):
+  #     return jax.grad(xc_fn, argnums=1)(rho, r)
+
+  #   g = jax.vmap(grad)(r)
+  #   self.assertFalse(jnp.isnan(g).any())
 
 
 if __name__ == "__main__":
