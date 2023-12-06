d765048a8bd467fde57863f670c5b4e5b7d5edb2
mavenlin <mavenlin@gmail.com>
2023-12-06 18:36:09 +0800

    fix format

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# Copyright 2022 Garena Online Private Limited
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import jax
 import jax_xc
 import autofd.operators as o
